53a2629c6061b16ec027434d896421b930e93a5e
DevSlem <devslem12@gmail.com>
2024-05-13 06:00:15 +0000

    Update Knapsack Env

--- KnapsackEnv.py
+++ KnapsackEnv.py
@@ -1,6 +1,6 @@
 import numpy as np
-import gym 
-from gym import spaces
+import gymnasium as gym
+from gymnasium import spaces
 
 
 class KnapsackEnv(gym.Env):
@@ -17,51 +17,71 @@ class KnapsackEnv(gym.Env):
         self.values = self.items[:, 0]
         self.weights = self.items[:, 1]
         self.remaining_capacities = np.array(capacities)
-        self.selectable = np.ones(self.num_items)
         self.ratios = self.values / self.weights
-
         
         # 행동 공간
         self.action_space = np.array(self.num_items * self.n_bags)
 
         # 상태 공간
         # n_items x item_dim (profit, weight, q_value, selected, residual capacities
-        self.state = np.empty((self.num_items * self.n_bags, self.item_dim), dtype=np.float32)
+        # self.state = np.empty((self.num_items * self.n_bags, self.item_dim), dtype=np.float32)
         
         self.time_step = 0
         self.reset()
 
     def reset(self):
         # Initialize the state with the items and initial capacity
-        self.state = np.empty((self.num_items * self.n_bags, self.item_dim), dtype=np.float32)
+        self.selectable = np.ones((self.num_items, self.n_bags)).astype(bool)
+        # self.state = np.empty((self.num_items * self.n_bags, self.item_dim), dtype=np.float32)
+        self.remaining_capacities = self.capacities.copy()
 
         # Update each item's profit, weight, ratios, selectable, remaining capacities     
-        self.state[:, 0] = np.tile(self.values, self.n_bags)
-        self.state[:, 1] = np.tile(self.weights, self.n_bags)
-        self.state[:, 2] = np.tile(self.ratios, self.n_bags)
-        self.state[:, 3] = np.tile(np.ones(self.num_items), self.n_bags)
-        self.state[:, 4] = np.tile(self.capacities, self.num_items)
+        state = self._make_state()
         
         self.time_step = 0
-        return self.state[np.newaxis, :, :] #add batch dimension
+        return state[np.newaxis, :, :] #add batch dimension
 
     def step(self, action):
-        item_idx = action % self.num_items
-        bag_idx = action // self.num_items
+        # item_idx = action % self.num_items
+        # bag_idx = action // self.num_items
+        item_idx = action // self.n_bags
+        bag_idx = action % self.n_bags
         
         done = False
         reward = 0
 
-        if self.selectable[item_idx] == 1 and self.weights[item_idx] <= self.remaining_capacities[bag_idx]:
-            self.selectable[item_idx] = 0
+        if self.selectable[item_idx, bag_idx] == True and self.weights[item_idx] <= self.remaining_capacities[bag_idx]:
+            self.selectable[item_idx, :] = False
             self.remaining_capacities[bag_idx] -= self.weights[item_idx]
             reward += self.values[item_idx]
 
+        # set selectable
+        self._update_selctable_matrix()
+        
         self.time_step += 1
         # Check if the problem is done (either by running out of capacity or if all items are considered)
-        if self.time_step >= self.num_items or np.all(self.remaining_capacities <= 0):
+        if np.all(~self.selectable):
             done = True
 
         # set the 'state'
-        next_state = self.state[np.newaxis, :, :]  # Add batch dimension
-        return next_state, reward, done, {}
+        next_state = self._make_state()  # Add batch dimension
+        return next_state[np.newaxis, ...], reward, done, {}
+    
+    def _make_state(self):
+        state = np.empty((self.num_items * self.n_bags, self.item_dim), dtype=np.float32)
+        state[:, 0] = np.repeat(self.values, self.n_bags)
+        state[:, 1] = np.repeat(self.weights, self.n_bags)
+        state[:, 2] = np.repeat(self.ratios, self.n_bags)
+        state[:, 3] = self.selectable.astype(np.float32).flatten()
+        state[:, 4] = np.tile(self.remaining_capacities, self.num_items)
+        return state
+    
+    def _update_selctable_matrix(self):
+        # selectable: (n_items, n_bags)
+        # weights: (n_items,)
+        # remaining_capacities: (n_bags,)
+        # broadcast weights and remaining_capacities to (n_items, n_bags)
+        # then, compare with selectable
+        temp = self.weights[..., np.newaxis] <= self.remaining_capacities[np.newaxis, ...]
+        self.selectable = np.logical_and(self.selectable, temp)
+        
