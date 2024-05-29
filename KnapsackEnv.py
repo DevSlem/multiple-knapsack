5dab1c089a1e70d11167e78f4365272d8c290c4d
succulentum <Officialjsy01@gmail.com>
2024-05-29 18:49:43 +0900

    fixed reset() to generate random inputs.

--- KnapsackEnv.py
+++ KnapsackEnv.py
@@ -4,45 +4,75 @@ from gymnasium import spaces
 
 
 class KnapsackEnv(gym.Env):
-    def __init__(self, items, capacities):
+    def __init__(self, items = None, capacities = None):
         super(KnapsackEnv, self).__init__()
         
         self.item_dim = 5
-        self.items = np.array(items) # 아이템의 값, 무게
-        self.capacities = np.array(capacities).astype(np.float32) #knapsack의 capacity
         
-        self.num_items = len(items)
-        self.num_bags = len(capacities)
-                     
-        #normalization
-        self.values = self.items[:, 0].astype(np.float32)
-        self.max_item_value = np.max(self.items[:, 0])
-        self.values /= self.max_item_value
-        
-        self.weights = self.items[:, 1].astype(np.float32)
-        self.max_item_weight = np.max(self.items[:, 1])
-        self.weights /= self.max_item_weight
-        
-        self.capacities /= self.max_item_weight
-        
-        self.remaining_capacities = np.array(capacities)
-        self.ratios = self.values / self.weights
-        
-        # 행동 공간
-        self.action_space = np.array(self.num_items * self.num_bags)
+        if items is not None and capacities is not None:
+            self.items = np.array(items) # 아이템의 값, 무게
+            self.capacities = np.array(capacities).astype(np.float32) #knapsack의 capacity
+            self.num_items = len(items)
+            self.num_bags = len(capacities)
+        else:
+            self.items = None 
+            self.capacities = None 
+            self.num_items = 0
+            self.num_bags = 0 
 
+        #self.values = self.items[:, 0].astype(np.float32)
+        #self.max_item_value = np.max(self.items[:, 0])
+        
+        #self.weights = self.items[:, 1].astype(np.float32)
+        #self.max_item_weight = np.max(self.items[:, 1])
+                    
         # 상태 공간
         # n_items x item_dim (profit, weight, q_value, selected, residual capacities
         # self.state = np.empty((self.num_items * self.n_bags, self.item_dim), dtype=np.float32)
         
         self.time_step = 0
-        self.reset()
+        self.reset(items, capacities)
 
-    def reset(self):
+    def reset(self, items=None, capacities=None):
+        # generate problem that has different(random) item num and bag num... 
+        # num_items : between 2 and 100
+        # num_bags : ?? 10-30
+        # profit, weights [10^-6, 1], normalized      
+            
+        if self.items is None or self.capacities is None:       #Random Generation
+            max_value = 10
+            max_weight = 10
+            max_capacity = 10
+            
+            self.num_items = np.random.randint(2, 101)
+            self.num_bags = np.random.randint(10, 30)
+            
+            self.values = np.random.randint(1, max_value + 1, self.num_items).astype(np.float32)
+            self.weights = np.random.randint(1, max_weight + 1, self.num_items).astype(np.float32)
+            self.max_item_value = np.max(self.values)
+            self.max_item_weight = np.max(self.weights)
+            self.values /= self.max_item_value
+            self.weights /= self.max_item_weight
+            self.ratios = self.values / self.weights
+            
+            self.capacities = np.random.randint(1, max_capacity + 1, self.num_bags).astype(np.float32)       
+            self.capacities /= self.max_item_weight
+            self.remaining_capacities = np.array(self.capacities)
+        else:
+            self.values = self.items[:, 0].astype(np.float32)
+            self.max_item_value = np.max(self.items[:, 0])
+            self.weights = self.items[:, 1].astype(np.float32)
+            self.max_item_weight = np.max(self.items[:, 1])
+            self.values /= self.max_item_value
+            self.weights /= self.max_item_weight
+            self.ratios = self.values / self.weights
+            self.remaining_capacities = np.array(self.capacities)
+            
+        self.action_space = np.array(self.num_items * self.num_bags)
+        
         # Initialize the state with the items and initial capacity
         self.selectable = np.ones((self.num_items, self.num_bags)).astype(bool)
         # self.state = np.empty((self.num_items * self.n_bags, self.item_dim), dtype=np.float32)
-        self.remaining_capacities = self.capacities.copy()
 
         # Update each item's profit, weight, ratios, selectable, remaining capacities     
         state = self._make_state()
