8ac0a3a1f606dc6612e7837df580974af6f365ce
succulentum <Officialjsy01@gmail.com>
2024-05-30 23:37:21 +0900

    added normalization just before the returns

--- KnapsackEnv.py
+++ KnapsackEnv.py
@@ -19,12 +19,6 @@ class KnapsackEnv(gym.Env):
             self.capacities = None 
             self.num_items = 0
             self.num_bags = 0 
-
-        #self.values = self.items[:, 0].astype(np.float32)
-        #self.max_item_value = np.max(self.items[:, 0])
-        
-        #self.weights = self.items[:, 1].astype(np.float32)
-        #self.max_item_weight = np.max(self.items[:, 1])
                     
         # 상태 공간
         # n_items x item_dim (profit, weight, q_value, selected, residual capacities
@@ -37,37 +31,28 @@ class KnapsackEnv(gym.Env):
         # generate problem that has different(random) item num and bag num... 
         # num_items : between 2 and 100
         # num_bags : ?? 10-30
-        # profit, weights [10^-6, 1], normalized      
-            
-        if self.items is None or self.capacities is None:       #Random Generation
+        # profit, weights [10^-6, 1], normalization will be done just before return     
+        
+        if self.items is None or self.capacities is None:           #Random Generation
             max_value = 10
             max_weight = 10
             max_capacity = 20
             
             self.num_items = np.random.randint(2, 151)
             self.num_bags = np.random.randint(2, 31)
-            
-            self.values = np.random.randint(1, max_value + 1, self.num_items).astype(np.float32)
-            self.weights = np.random.randint(1, max_weight + 1, self.num_items).astype(np.float32)
-            self.max_item_value = np.max(self.values)
-            self.max_item_weight = np.max(self.weights)
-            self.values /= self.max_item_value
-            self.weights /= self.max_item_weight
-            self.ratios = self.values / self.weights
-            
-            self.capacities = np.random.randint(1, max_capacity + 1, self.num_bags).astype(np.float32)       
-            self.capacities /= self.max_item_weight
-            self.remaining_capacities = np.array(self.capacities)
-        else:
-            self.values = self.items[:, 0].astype(np.float32)
-            self.max_item_value = np.max(self.items[:, 0])
-            self.weights = self.items[:, 1].astype(np.float32)
-            self.max_item_weight = np.max(self.items[:, 1])
-            self.values /= self.max_item_value
-            self.weights /= self.max_item_weight
-            self.ratios = self.values / self.weights
-            self.remaining_capacities = np.array(self.capacities)
-            
+            self.values = np.random.randint(1, max_value + 1, self.num_items)
+            self.weights = np.random.randint(1, max_weight + 1, self.num_items)
+            self.capacities = np.random.randint(1, max_capacity + 1, self.num_bags)                        
+        else:                                                       #Non Random 
+            self.values = self.items[:, 0]
+            self.weights = self.items[:, 1]
+        
+        self.ratios = self.values / self.weights
+        self.remaining_capacities = np.array(self.capacities) 
+        
+        self.max_item_value = np.max(self.values)
+        self.max_item_weight = np.max(self.weights)      
+        
         self.action_space = np.array(self.num_items * self.num_bags)
         
         # Initialize the state with the items and initial capacity
@@ -76,23 +61,24 @@ class KnapsackEnv(gym.Env):
 
         # Update each item's profit, weight, ratios, selectable, remaining capacities     
         state = self._make_state()
-        
+        norm_state = self._normalize_state(state) 
         self.time_step = 0
-        return state[np.newaxis, :, :] #add batch dimension
+        
+        return norm_state[np.newaxis, :, :] #add batch dimension
 
     def step(self, action):
-        # item_idx = action % self.num_items
-        # bag_idx = action // self.num_items
         item_idx = action // self.num_bags
         bag_idx = action % self.num_bags
         
         done = False
         reward = 0
+        norm_reward = 0
 
         if self.selectable[item_idx, bag_idx] == True and self.weights[item_idx] <= self.remaining_capacities[bag_idx]:
             self.selectable[item_idx, :] = False
             self.remaining_capacities[bag_idx] -= self.weights[item_idx]
             reward += self.values[item_idx]
+            norm_reward = np.float32(reward) / np.float32(self.max_item_weight) #normalization
 
         # set selectable
         self._update_selctable_matrix()
@@ -104,7 +90,9 @@ class KnapsackEnv(gym.Env):
 
         # set the 'state'
         next_state = self._make_state()  # Add batch dimension
-        return next_state[np.newaxis, ...], reward, done, {"value": reward * self.max_item_value}
+        norm_next_state = self._normalize_state(next_state)
+        
+        return norm_next_state[np.newaxis, ...], norm_reward, done, {"value": reward}
     
     def _make_state(self):
         state = np.empty((self.num_items * self.num_bags, self.item_dim), dtype=np.float32)
@@ -123,4 +111,13 @@ class KnapsackEnv(gym.Env):
         # then, compare with selectable
         temp = self.weights[..., np.newaxis] <= self.remaining_capacities[np.newaxis, ...]
         self.selectable = np.logical_and(self.selectable, temp)
+    
+    def _normalize_state(self, state):
+        normstate = state.copy()
+        normstate[:, 0] /= self.max_item_value #value
+        normstate[:, 1] /= self.max_item_weight #weight
+        #ratio is same
+        #selectable is same
+        normstate[:, 4] /= self.max_item_weight #remaining_capacities
+        return normstate
         
